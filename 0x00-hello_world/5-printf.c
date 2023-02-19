884dbf3dceb8f3f73ee42d0b076fc0d4174d1ec9
menyagah <menyagah27@gmail.com>
2023-02-19 22:03:36 +0300

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("Roses are red,\n");
+  printf("Violets are blue,\n");
+  printf("I'm terrible at poetry,\n");
+  printf("But I can make art with C.\n");
+
+  return 0;
+}
