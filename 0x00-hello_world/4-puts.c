7dfe199144e4bb45435aa9116fcd339834626684
menyagah <menyagah27@gmail.com>
2023-02-19 09:55:28 -0800

    Write a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
@@ -9,3 +10,4 @@ int main(void)
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
+
