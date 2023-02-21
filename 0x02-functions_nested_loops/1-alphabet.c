26fa0774323f867c5c1ef96f8d8fe389c2369563
menyagah <menyagah27@gmail.com>
2023-02-21 19:40:40 +0300

    Write a program that prints a - z in small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char c = 'a';
+while(c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+/**
+ * print_alphabet - function prototype for print_alphabet
+ *
+ */
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif /* MAIN_H */
