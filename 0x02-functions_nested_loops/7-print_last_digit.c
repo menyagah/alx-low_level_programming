ca5e3e49c1ea2ed06dbb41f3dd4dd333069285cb
menyagah <menyagah27@gmail.com>
2023-02-22 19:56:37 +0300

    change syntax to pass failing test

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,10 @@
  */
 int print_last_digit(int num)
 {
-int last_digit;
-if (num < 0)
+int last_digit =  num % 10;
+if(last_digit < 0)
 {
-last_digit = -num % 10;
-}
-else
-{
-last_digit = num % 10;
+last_digit *= -1;
 }
 _putchar(last_digit + '0');
 return (last_digit);
