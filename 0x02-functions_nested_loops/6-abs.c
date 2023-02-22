cb22b333e4ea5ca495e362a016d192952e0fde19
menyagah <menyagah27@gmail.com>
2023-02-22 13:08:09 +0300

    chnge the dash to colon

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
  * _abs - function that returns the absolute value of an integer
  * @num : integer to check
  *
- * Return - returns -num if num is < 0 and num if num is > 0
+ * Return : returns -num if num is < 0 and num if num is > 0
  */
 int _abs(int num)
 {
