1ecaff0d74a85bec20834a9e3f42bcec89379749
menyagah <menyagah27@gmail.com>
2023-02-22 18:33:37 +0300

    change syntax

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,12 +3,6 @@
 
 /**
  * print_alphabet - a function prototype for print_alphabet
- * print_alphabet_x10  - a function prototype for printing alphabets a-z 10 times
- * _islower - a function prototype that checks for lowercase characters
- * _isalpha - a function prototype that checks for alphabetic character
- * print_sign - a function prototype that prints the sign of a number
- * _abs - a function prototype that computes the absolute value of an integer
- * print_last_digit - a function prototype that prints the last digit of a number
  */
 int _putchar(char c);
 void print_alphabet(void);
