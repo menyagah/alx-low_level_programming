0418532cd4427cc9bd64ca56270bfb662e8c9fbd
menyagah <menyagah27@gmail.com>
2023-02-22 21:07:33 +0300

    Add natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_to_98 - a function that prints natural numbers to 98
+ * @n: value to be used
+ *
+ */
+int print_to_98(int n)
+{
+while (n != 98)
+{
+printf(n);
+if(n < 98)
+{
+n++;
+}
+else
+{
+n--;
+}
+}
+printf(n);
+}
