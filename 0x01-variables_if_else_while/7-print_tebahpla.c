88153db6977389a4a1edff435b76de9cb22546a7
menyagah <menyagah27@gmail.com>
2023-02-23 18:51:27 +0300

    A program printing alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - code starts here
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+char a = 'a';
+char z = 'z';
+char i;
+for (i = a; i <= z; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+i}
