188d2f90df8c46dc456055c35e5035796c7217de
menyagah <menyagah27@gmail.com>
2023-02-24 05:32:43 +0300

    fix betty c errors

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,20 @@
  * Return: return 0
  */
 
-int main() {
-  int i;
+int main() 
+{
+int i;
   
-  for(i = 0; i < 10; i++) {
-    putchar(i + '0');
-    if(i < 9) {
-      putchar(',');
-      putchar(' ');
-    }
-  }
-  putchar('\n');
-  return 0;
+for(i = 0; i < 10; i++) 
+{
+putchar(i + '0');
+if(i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return 0;
 }
 
