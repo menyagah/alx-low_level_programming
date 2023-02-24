0c77232e64b886cacc2522fdfafee71a3271787b
menyagah <menyagah27@gmail.com>
2023-02-24 05:24:41 +0300

    Change syntax

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,20 +7,19 @@
  */
 
 int main(void) {
-int i, j;
-for (i = 0; i < 10; i++)
-{
-putchar(i + '0');
-
-for (j = i + 1; j < 10; j++)
-{
-putchar(',');
-putchar(i + '0');
-putchar(j + '0');
+  int i, j, k;
+  
+  for(i = 0; i < 10; i++) {
+    for(j = i + 1; j < 10; j++) {
+      for(k = j + 1; k < 10; k++) {
+        putchar(i + '0');
+        putchar(j + '0');
+        putchar(k + '0');
+        putchar(',');
+      }
+    }
+  }
+  
+  return 0;
 }
-}
-
-return 0;
-}
-
 
