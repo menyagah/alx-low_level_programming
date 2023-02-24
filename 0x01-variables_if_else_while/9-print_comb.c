c6495b3611de0936048ee213f2b5650d853404a9
menyagah <menyagah27@gmail.com>
2023-02-24 05:04:13 +0300

    Change syntax

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,28 +6,22 @@
  * Return: return 0
  */
 
-int main(void)
-{
+int main(void) {
 int i, j;
-
 for (i = 0; i < 10; i++)
 {
-if (i != 9)
-{
-putchar(',');
-}
+putchar(i + '0');
+
 for (j = i + 1; j < 10; j++)
 {
+putchar(',');
+putchar(' ');
 putchar(i + '0');
 putchar(j + '0');
-if (i != 9 || j != 8)
-{
-putchar(',');
 }
-}
-putchar(i + '0');
 }
 
 return 0;
 }
+
 
