39c98956c4f84edd53f0ccfd0c096c78d13def54
menyagah <menyagah27@gmail.com>
2023-02-25 13:08:11 +0300

    change syntax

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,15 +5,12 @@
  * main - tests the positive_or_negative() function
  * Return: 0
  */
+
 int main(void)
 {
 int i = 0;
+
 positive_or_negative(i);
-if (i == 0)
-{
 printf("%d is zero\n", i);
-}
-printf("%d is not zero\n", i);
 return (0);
 }
-
