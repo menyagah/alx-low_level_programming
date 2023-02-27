199d85e8fe1936548dddcee93ae8426945c19274
menyagah <menyagah27@gmail.com>
2023-02-27 15:40:21 +0300

    add readme and first function that takes a pointer to an int as parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - start of the function
+ *@n: the parameter to be passed
+ *
+ * Return: returns 0
+ */
+
+int reset_to_98(int *n)
+{
+	n = 98;
+	printf("value of 'n': %d\n", n);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+* positive_or_negative - a function prototype to print positive or negative numbers
+*/
+
+void reset_to_98(int *n);
+#endif /* MAIN_H */
