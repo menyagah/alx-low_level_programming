27fa4407a03a2548a59512eb6e005723e4a8d6a3
menyagah <menyagah27@gmail.com>
2023-02-28 02:24:36 +0300

    function to copy string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - start of the program
+ * @dest: parameter
+ * @src: parameter
+ *
+ * Return: returns destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return dest;
+}
