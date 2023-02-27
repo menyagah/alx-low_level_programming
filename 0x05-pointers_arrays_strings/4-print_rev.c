c050860cdad578135be1a00b9c517419d76ec20d
menyagah <menyagah27@gmail.com>
2023-02-27 17:34:42 +0300

    Modify header to have protype function and add function to print the string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function to print the string passed in in reverse
+ * @s - the string argument
+ *
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	while (len > 0)
+	{
+		printf("%c", *s);
+		s--;
+		len--;
+	}
+	printf9"\n");
+}
