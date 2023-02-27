d6ed8b0c2bbe69f6a8c24fd204549a43b74a34ed
menyagah <menyagah27@gmail.com>
2023-02-28 01:46:51 +0300

    Add program to print half the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half the string
+ * @str: parameter to be passed
+ *
+ */
+
+void puts_half(char *str) 
+{
+	int length = strlen(str);
+	int half = length / 2;
+	int i;
+
+	if(length % 2 == 1)
+	{
+		half = (length -1) / 2;
+	}
+	for (i = half; i < length; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
