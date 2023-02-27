5478ad606e8345c9d544297a66818aea0c149f2b
menyagah <menyagah27@gmail.com>
2023-02-27 17:19:46 +0300

    remove return

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,11 +4,9 @@
  * _puts - a function that prints a string and outputs a new line
  * @str: the argument string
  *
- * Return: returns zero
  */
 
 void _puts(char *str)
 {
 	printf("%s\n", str);
-	return (0);
 }
