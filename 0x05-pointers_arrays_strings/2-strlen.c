49f603bad9d99a353c16a2dc4efe6b488382a5cc
menyagah <menyagah27@gmail.com>
2023-02-27 17:02:12 +0300

    modify header to take prototype function and add the function to count string characters

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the length of the passed argument string
+ * @s: the passed string argument
+ *
+ * Return: returns len
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*s != '\0') 
+	{
+		len++;
+		s++;
+	}
+	return len;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,9 +3,10 @@
 
 #include <stdio.h>
 /**
-* positive_or_negative - a function prototype to print positive or negative numbers
+* reset_to_98 - a function prototype to print the result as 98
 */
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif /* MAIN_H */
