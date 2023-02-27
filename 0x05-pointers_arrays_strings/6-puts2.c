816e610fd0c87e3164fc44aca38cd910b9b787f8
menyagah <menyagah27@gmail.com>
2023-02-28 01:20:24 +0300

    A function that prints the even value of a character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - prints the even value
+ * @str - the parameter being passed in as a string
+ *
+ */
+
+void puts2(char *str)
+{
+	for(int i = 0; i < strlen(str); i+=2) 
+	{
+		printf("%c\n", str[i]);
+	}
+}
