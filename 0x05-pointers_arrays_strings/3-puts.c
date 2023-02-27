0e1c5ef4c55cf870247418f9ebe263f15443bec0
menyagah <menyagah27@gmail.com>
2023-02-27 17:14:38 +0300

    Add a function that prints a string with a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string and outputs a new line
+ * @str: the argument string
+ *
+ * Return: returns zero
+ */
+
+void _puts(char *str)
+{
+	printf("*str\n");
+	return (0);
+}
