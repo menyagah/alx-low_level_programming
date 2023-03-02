40b67a7b116ba0af1eb6fdb173c8bbcf51a019c3
menyagah <menyagah27@gmail.com>
2023-03-02 12:55:12 +0300

    Add function

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncat - start of the function
+ * @dest: parameter one
+ * @src: parameter two
+ * @n parameter three
+ *
+ * Return: returns start
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *start = dest;
+
+	while (*dest != '\0') 
+	{
+		dest++;
+	}
+
+	int i = 0;
+
+	while(*src != '\0' && i < n)
+	{
+		*dest++ = *src++;
+		i++;
+	}
+
+	*dest = '\0';
+	return (start);
+}
