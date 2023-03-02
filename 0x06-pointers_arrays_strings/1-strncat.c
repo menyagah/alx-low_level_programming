1657959547436587ca2f89b3b66cd447fb8db026
menyagah <menyagah27@gmail.com>
2023-03-02 13:06:12 +0300

    change syntax

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,23 +11,12 @@
  * Return: returns start
  */
 
-char *_strncat(char *dest, char *src, int n)
-{
-	char *start = dest;
-
-	while (*dest != '\0') 
-	{
-		dest++;
-	}
-
-	int i = 0;
-
-	while (*src != '\0' && i < n)
-	{
-		*dest++ = *src++;
-		i++;
-	}
-
-	*dest = '\0';
-	return (start);
+char *_strncat(char *dest, char *src, int n) {
+    int dest_len = strlen(dest);
+    int i;
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dest[dest_len + i] = src[i];
+    }
+    dest[dest_len + i] = '\0';
+    return dest;
 }
