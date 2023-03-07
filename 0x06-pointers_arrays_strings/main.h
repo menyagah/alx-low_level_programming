41313384b203660a074ae2fb817b3a57515867a7
menyagah <menyagah27@gmail.com>
2023-03-07 19:37:47 +0300

    fix typo on main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,6 @@
 * _strcat - a function prototype to concatnat two strings
 */
 
-char* _strcat(char* dest, char* src);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 #endif /* MAIN_H */
