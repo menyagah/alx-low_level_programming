cc4876a1b7b0f037d7d15e8270af2fa144a976ec
menyagah <menyagah27@gmail.com>
2023-03-07 20:34:35 +0300

    change syntax

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,18 +12,16 @@ char *_strcat(char *dest, char *src)
 {
 	char *end_of_dest = dest;
 	
-	while ( *end_of_dest != '\0' )
+	while (*end_of_dest != '\0')
 	{
 		end_of_dest++;
 	}
-	while ( *src != '\0' )
+	while (*src != '\0')
 	{
 		*end_of_dest = *src;
 		end_of_dest++;
 		src++;
 	}
-
 	*end_of_dest = '\0';
-
 	return (dest);
 }
