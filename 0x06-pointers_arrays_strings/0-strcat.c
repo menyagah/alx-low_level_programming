a2454187eb419d11fb7d916c94a9215ce7797cc6
menyagah <menyagah27@gmail.com>
2023-03-09 09:01:26 +0300

    Add space around the equals sign

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,7 +19,7 @@ char *_strcat(char *dest, char *src)
 	}
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[dest_len + i]=src[i];
+		dest[dest_len + i] = src[i];
 	}
 	dest[dest_len + i]='\0';
 	return (dest);
