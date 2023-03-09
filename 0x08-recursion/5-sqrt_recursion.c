2dc75e5a4b1920487f0b4a0af855a52312ce7554
menyagah <menyagah27@gmail.com>
2023-03-09 19:38:07 +0300

    Fix betty c errors

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,19 +10,24 @@
 
 int _sqrt_helper(int n, int i);
 
-int _sqrt_recursion(int n) {
-    if (n < 0) {
-        return -1;
-    }
-    return _sqrt_helper(n, 0);
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return -1;
+	}
+	return _sqrt_helper(n, 0);
 }
 
-int _sqrt_helper(int n, int i) {
-    if (i * i == n) {
-        return i;
-    }
-    if (i * i > n) {
-        return -1;
-    }
-    return _sqrt_helper(n, i + 1);
+int _sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+	{
+		return i;
+	}
+	if (i * i > n)
+	{
+		return -1;
+	}
+	return _sqrt_helper(n, i + 1);
 }
