e7856a3d85f5f4eccd0952cebc3e880a81f8aa5d
menyagah <menyagah27@gmail.com>
2023-03-09 15:06:26 +0300

    Removing trailing white space and space after brace

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,16 +5,18 @@
  * _pow_recursion - a function that returns the value of x raised to power of y
  * @x: parameter one
  * @y: the value to be raised
+ *
+ * Return: returns the value of x to the power of y
  */
 
 int _pow_recursion(int x, int y)
 {
-	if(y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
 	else if (y == 0)
-	{ 
+	{
 		return (1);
 	}
 	else
