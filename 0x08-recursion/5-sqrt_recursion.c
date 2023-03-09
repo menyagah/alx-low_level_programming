3db40f41025cfca8db36149aaafea109788a6dd2
menyagah <menyagah27@gmail.com>
2023-03-09 19:32:33 +0300

    Change syntax

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,33 +8,21 @@
  * Return: returns the natural square root
  */
 
-int _sqrt_recursion(int n)
-{
-	int low = 1, high = n, mid;
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0 || n == 1) 
-	{
-		return n;
-	}
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
+int _sqrt_helper(int n, int i);
 
-		if (mid * mid == n)
-		{
-			return mid;
-		}
-		else if (mid * mid < n)
-		{
-			low = mid + 1;
-		}
-		else
-		{
-			high = mid -1;
-		}
-	}
-	return -1;
+int _sqrt_recursion(int n) {
+    if (n < 0) {
+        return -1; // Negative number doesn't have a natural square root
+    }
+    return _sqrt_helper(n, 0); // Start with i = 0
+}
+
+int _sqrt_helper(int n, int i) {
+    if (i * i == n) {
+        return i; // Found the natural square root
+    }
+    if (i * i > n) {
+        return -1; // Not a perfect square
+    }
+    return _sqrt_helper(n, i + 1); // Continue with i + 1
 }
