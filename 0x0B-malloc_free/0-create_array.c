cbe3c229e449bd3f3c2445282a185067e8ad26a3
menyagah <menyagah27@gmail.com>
2023-03-15 00:16:22 +0300

    format document

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,21 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *arr;
+  char *arr;
+  unsigned int i;
 
-    if (size == 0)
-  {
-      return (NULL);
-  }
-    arr = malloc(size * sizeof(char));
-    if (arr == NULL)
+  if (size == 0)
   {
     return (NULL);
   }
-    for (unsigned int i = 0; i < size; i++)
+  arr = malloc(size * sizeof(char));
+
+  if (arr == NULL)
+  {
+    return (NULL);
+  }
+
+  for (i = 0; i < size; i++)
   {
     arr[i] = c;
   }
