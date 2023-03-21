140af438b4a3d5acb05e63f1bb5f5638f869f383
menyagah <menyagah27@gmail.com>
2023-03-21 16:42:15 +0300

    Add function to print the function name and path

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point. Prints the name of the program, followed by a new line.
+ *
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the strings which are those arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
