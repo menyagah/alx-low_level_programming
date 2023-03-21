8e28c8842e32cd6fe41789484afebe00dcbe4cf0
menyagah <menyagah27@gmail.com>
2023-03-21 16:43:20 +0300

    Add header

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point. Prints the name of the program, followed by a new line.
@@ -11,6 +11,6 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return (0);
+  printf("%s\n", argv[0]);
+  return (0);
 }
