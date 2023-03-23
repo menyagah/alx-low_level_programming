116dd06e18e64f6a5b776d995708d39e183f358e
menyagah <menyagah27@gmail.com>
2023-03-23 21:16:14 +0300

    Fix header betty c issues

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,23 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/**
+ * print_name - Prints a name in uppercase
+ *
+ * @name: Name to print
+ * @f: Function pointer to print the name
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *));
+
+/**
+ * print - Prints a character
+ *
+ * @c: Character to print
+ *
+ * Return: void
+ */
 void print(char *c);
 
 #endif /* MAIN_H */
