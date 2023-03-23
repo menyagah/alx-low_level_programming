e4ee46c0fc341dcbb470335915b84f7bf3e03d95
menyagah <menyagah27@gmail.com>
2023-03-23 21:36:55 +0300

    End file with a new line

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "function_pointers.h"
 
 /**
  * print_name - Prints a name
@@ -13,7 +12,7 @@ void print_name(char *name, void (*f)(char *))
 {
 if (name != NULL && f != NULL)
 {
-printf("Name: ");
+printf("Name: \n");
 (*f)(name);
 }
 }
