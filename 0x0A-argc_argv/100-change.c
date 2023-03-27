1454ef490ccf2f5af64f77991c4b651fd6137fb6
menyagah <menyagah27@gmail.com>
2023-03-27 19:14:52 +0300

    fix betty c errors

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,14 +15,13 @@ int main(int argc, char *argv[])
 int cents, count = 0;
 int coins[] = {25, 10, 5, 2, 1};
 int i;
+cents = atoi(argv[1]);
 
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-
-cents = atoi(argv[1]);
 
 if (cents < 0)
 {
