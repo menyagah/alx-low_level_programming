21b1382161587d7f033fe6cf9b9342f6dc632a98
menyagah <menyagah27@gmail.com>
2023-03-27 11:16:29 +0300

    move declarations of num1, num2, and result are at the beginning of the block

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,18 +11,18 @@
 
 int main(int argc, char **argv)
 {
+	int num1, num2, result;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
 	printf("%d\n", result);
 
 	return (0);
 }
-
