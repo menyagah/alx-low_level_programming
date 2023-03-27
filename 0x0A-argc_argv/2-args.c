ec1b9a2b7db881eb0623f44ef31d78d64f61726c
menyagah <menyagah27@gmail.com>
2023-03-27 08:50:32 +0300

    A program that will print all arguments passed in including the first one

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point. Prints the name of the program, followed by a new line.
+ *
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the strings which are those arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
