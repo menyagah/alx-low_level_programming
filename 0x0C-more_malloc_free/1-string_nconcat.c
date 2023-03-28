811ab0beae3a24dbd9d10b3a310700e9e993b758
menyagah <menyagah27@gmail.com>
2023-03-29 02:09:29 +0300

    Fix compile errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * string_nconcat - concatenates two strings
@@ -12,21 +13,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	unsigned int len1, len2;
+	char *result;
 
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
 	if (n >= len2)
+	{
 		n = len2;
+	}
 
-	char *result = malloc(sizeof(char) * (len1 + n + 1));
+	result = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (result == NULL)
+	{
 		return (NULL);
+	}
 
 	memcpy(result, s1, len1);
 	memcpy(result + len1, s2, n);
@@ -34,4 +46,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	return (result);
 }
-
