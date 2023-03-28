03a4d898550c1e06f4da2dfef8169ac60c3cd725
menyagah <menyagah27@gmail.com>
2023-03-29 02:36:01 +0300

    Remove spaces at the start of line

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,21 +12,20 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  void *ptr;
+void *ptr;
 
-  if (nmemb == 0 || size == 0)
-  {
-    return (NULL);
-  }
-
-  ptr = malloc(nmemb * size);
-
-  if (ptr == NULL)
-  {
-    return (NULL);
-  }
-
-  memset(ptr, 0, nmemb * size);
-  return (ptr);
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
 }
 
+ptr = malloc(nmemb * size);
+
+if (ptr == NULL)
+{
+return (NULL);
+}
+
+memset(ptr, 0, nmemb * size);
+return (ptr);
+}
