eab7472a8a703b19ab4901df434c9dae8dbdcb6d
menyagah <menyagah27@gmail.com>
2023-03-29 01:52:55 +0300

    Fix betty c errors

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,11 +3,9 @@
 #include <stdio.h>
 
 /**
- * Allocates a block of memory of the given size using malloc.
- * If the allocation fails, terminates the process with an exit status of 98.
- *
- * @param b the size of the memory block to allocate.
- * @return a pointer to the allocated memory block.
+ * malloc_checked - Allocates a block of memory of the given size using malloc.
+ * @b: the size of the memory block to allocate.
+ * @return: a pointer to the allocated memory block.
  */
 
 void *malloc_checked(unsigned int b)
