d1edac7ca9c77f1357923a3d928ab57f4cacc18c
menyagah <menyagah27@gmail.com>
2023-03-31 00:18:47 +0300

    refactor code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,27 @@
 #include "lists.h"
 
-
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: A pointer to the head of the list_t list.
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list_t list
  *
- * Return: The number of nodes in the list_t list.
+ * Return: the number of nodes in the list_t list
  */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
+    size_t count = 0;
 
-while (h != NULL)
-{
-printf("[%ld] %s\n", h->len, (h->str != NULL) ? h->str : "(nil)");
-h = h->next;
-count++;
-}
+    while (h != NULL)
+    {
+        printf("[%d] ", h->len);
+        if (h->str == NULL)
+            printf("(nil)\n");
+        else
+            printf("%s\n", h->str);
 
-return (count);
+        h = h->next;
+        count++;
+    }
+
+    return (count);
 }
 
