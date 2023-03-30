21237b277b7a3f23de6cdf0147db0f3e0d0c90ed
menyagah <menyagah27@gmail.com>
2023-03-31 00:21:15 +0300

    Fix betty c errors and warnings

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,22 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("[%d] ", h->len);
-        if (h->str == NULL)
-            printf("(nil)\n");
-        else
-            printf("%s\n", h->str);
+while (h != NULL)
+{
+printf("[%d] ", h->len);
+if (h->str == NULL)
+{
+printf("(nil)\n");
+}
+else
+printf("%s\n", h->str);
 
-        h = h->next;
-        count++;
-    }
+h = h->next;
+count++;
+}
 
-    return (count);
+return (count);
 }
 
