c6ae1fbb9e2e32c70e12128741a7398badba495a
menyagah <menyagah27@gmail.com>
2023-03-30 23:31:34 +0300

    Add readme,header file and a function to prints the elements of a list_t

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - Singly linked list
+ * @str: String (malloc'ed string)
+ * @len: Length of the string
+ * @next: Pointer to the next node
+ *
+ * Description: Singly linked list node structure.
+ */
+typedef struct list_s
+{
+char *str;
+size_t len;
+struct list_s *next;
+} list_t;
+
+#endif /* LISTS_H */
+
